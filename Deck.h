cc1ef458d743d2f020ace62f5d7df6f0817150a5
chrishnwong <chrishnwong@gmail.com>
2018-12-01 20:30:09 -0500

    small formatting fixes and getsize implemented

--- Deck.h
+++ Deck.h
@@ -10,10 +10,10 @@ template <class C>
 class Deck
 {
     public:
-        void shuffle();
-        C* getNext();
-        bool isEmpty() const;
-        int getSize() const;
+        void shuffle(){random_shuffle(&items[0], &items[size-1]);};
+        C* getNext(){return items[current++];};
+        bool isEmpty() const{return current == size;};
+        int getSize() const{return size;};
 
     protected:
         int size;
@@ -21,21 +21,5 @@ class Deck
         C **items;
 
 };
-
-
-template <class C>
-void Deck<C>::shuffle(){
-    random_shuffle(&items[0], &items[size-1]);
-}
-
-template <class C>
-C* Deck<C>::getNext(){
-    return items[current++];
-}
-
-template <class C>
-bool Deck<C>::isEmpty() const{
-    return current == size;
-}
 
 #endif // DECK_H
