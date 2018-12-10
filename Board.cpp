7a8af958bd8af4ea892995959d29ed17af16aa4c
chrishnwong <chrishnwong@gmail.com>
2018-12-09 21:05:08 -0500

    Update to board tests

--- Board.cpp
+++ Board.cpp
@@ -44,20 +44,18 @@ int main(){
         cout << "A1 is flipped down" << endl;
     }
 
-    Card testcard = Card(FaceAnimal::Crab, FaceBackground::Red);
-    cout << "Creating a card with animal of crab and red background" << endl;
+    Deck<Card> cardDeck = CardDeck::make_CardDeck();
+    b.setCard(Letter::A,Number::1, cardDeck.getNext());
 
-    b.setCard(Letter::A, Number::1, *newCard)
-    cout << "Setting the new card to A1" << endl;
+    cout << "Setting the first card in a new Deck to A1" << endl;
 
-    if(b.getCard(Letter::A, Number::1)->getAnimal() == FaceAnimal::Crab){
-        cout << "The animal at A1 is a crab" << endl;
-    }
-    else{
-        cout << "The animal at A1 is not a crab, something is wrong" << endl;
-    }
+
+    cout << "The animal at A1 is enum: "<< b.getCard(Letter::A, Number::1)->getAnimal() << endl;
+    cout << "The background at A1 is enum: "<< b.getCard(Letter::A, Number::1)->getBackground() << endl;
+
 
     cout << "This concludes the public method tests for Board.cpp" << endl;
+    return 0;
 }
 #endif
 
