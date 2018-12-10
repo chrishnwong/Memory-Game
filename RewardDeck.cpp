3ab4c18e4ec5c2f377ef53d576bb71b82e87b817
chrishnwong <chrishnwong@gmail.com>
2018-12-09 21:50:36 -0500

    RewardDeck tests done

--- RewardDeck.cpp
+++ RewardDeck.cpp
@@ -1,4 +1,39 @@
 #include "RewardDeck.h"
+//#define REWARDDECK_TEST
+
+#ifdef REWARDDECK_TEST
+int main(){
+    cout << "creating Reward Deck" << endl;
+    Deck<Reward> rewardDeck = RewardDeck::make_RewardDeck();
+
+    cout<< "Unshuffled, the Rewards are as follows" << endl;
+    for(int i =0; i<7; i++){
+        cout << rewardDeck.getNext() << endl;
+    }
+
+    if(rewardDeck.isEmpty()){
+        cout << "Reward deck is empty" << endl;
+    }
+    else{
+        cout << "Reward deck is not empty, something is wrong" << endl;
+    }
+
+    rewardDeck = rewardDeck::make_RewardDeck();
+    cout << "Deck created successfully" << endl;
+
+
+    cout << "Shuffling, rewardDeck" << endl;
+    rewardDeck.shuffle();
+
+    cout<< "Shuffled, the Rewards are as follows" << endl;
+    for(int i =0; i<7; i++){
+        cout << rewardDeck.getNext() << endl;
+    }
+
+    cout << "This concludes the public method tests for RewardDeck.cpp" << endl;
+
+}
+#endif
 
 RewardDeck *RewardDeck::rewDeck = 0;
 
